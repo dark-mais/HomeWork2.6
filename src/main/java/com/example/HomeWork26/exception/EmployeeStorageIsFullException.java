0df74bd6087ee4797f0eb3c6307790d441e8fd24
dark-mais <dark1mais@gmail.com>
2024-02-18 19:11:14 +0300

    сделано ДЗ

---
+++ src/main/java/com/example/HomeWork26/exception/EmployeeStorageIsFullException.java
@@ -0,0 +1,11 @@
+package com.example.HomeWork26.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class EmployeeStorageIsFullException extends RuntimeException {
+    public EmployeeStorageIsFullException() {
+        super("Хранилище сотрудников заполнено");
+    }
+}
